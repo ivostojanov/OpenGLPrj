6c401069d33406850a99cdd9b096785f4a6416ed
Afro <ivostojanov51@gmail.com>
2021-05-14 20:28:33 +0200

    lab05_zad01 - branch creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,217 +1,271 @@
-#include <OpenGLPrj.hpp>
+#include gladglad.h
+#include GLFWglfw3.h
+#include stb_image.h
 
-#include <GLFW/glfw3.h>
+#include glmglm.hpp
+#include glmgtcmatrix_transform.hpp
+#include glmgtctype_ptr.hpp
 
-#include <Shader.hpp>
+#include learnopenglshader_m.h
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include <string>
+#include iostream
 
-const std::string program_name = ("Pac-Man Movement");
+void framebuffer_size_callback(GLFWwindow window, int width, int height);
+void processInput(GLFWwindow window);
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void processInput(GLFWwindow *window);
-
-// settings
+ settings
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-glm::mat4 position = glm::mat4(1.0f);
-
-std::string pacman_direction = "right";
-float speed = 0.5f;
-float rotation_angle = 0.0f;
-
-float delta_time = 0.0f;
-float previous_frame = glfwGetTime();
-
-
-int main() {
-    // glfw: initialize and configure
-      // ------------------------------
+int main()
+{
+     glfw initialize and configure
+     ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw window creation
-    // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, program_name.c_str(), nullptr, nullptr);
-    if (window == nullptr)
+     glfw window creation
+     --------------------
+    GLFWwindow window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, LearnOpenGL, NULL, NULL);
+    if (window == NULL)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
+        stdcout  Failed to create GLFW window  stdendl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
-    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+     glad load all OpenGL function pointers
+     ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        stdcout  Failed to initialize GLAD  stdendl;
         return -1;
     }
 
-    // build and compile our shader program
-    // ------------------------------------
-    Shader ourShader("../res/shaders/shader.vert",
-        "../res/shaders/shader.frag"
-    );
+     configure global opengl state
+     -----------------------------
+    glEnable(GL_DEPTH_TEST);
 
-  // set up vertex data (and buffer(s)) and configure vertex attributes
-    // ------------------------------------------------------------------
+     build and compile our shader zprogram
+     ------------------------------------
+    Shader ourShader(6.3.coordinate_systems.vs, 6.3.coordinate_systems.fs);
 
-  std::vector<float> vertices;
+     set up vertex data (and buffer(s)) and configure vertex attributes
+     ------------------------------------------------------------------
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-  float const PI_OVER_4 = glm::quarter_pi<float>();
-  float const PI = glm::pi<float>();
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
 
-  // Starting angle is not 0, but PI/8
-  float base_angle = PI/2.0f/9.0f/2.0f;
-  float angle = 3*base_angle;
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
 
-  for (auto i = 0; i < 3; i++)
-      vertices.push_back(0.0f);
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
 
-  float scale = 0.25f;
-  for (auto i = 0; i < (2*PI/base_angle)-5; i++) {
-      vertices.push_back(glm::cos(angle)*scale);
-      vertices.push_back(glm::sin(angle)*scale);
-      vertices.push_back(0.0f);
-      angle += base_angle;
-  }
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+     world space positions of our cubes
+    glmvec3 cubePositions[] = {
+        glmvec3( 0.0f,  0.0f,  0.0f),
+        glmvec3( 2.0f,  5.0f, -15.0f),
+        glmvec3(-1.5f, -2.2f, -2.5f),
+        glmvec3(-3.8f, -2.0f, -12.3f),
+        glmvec3( 2.4f, -0.4f, -3.5f),
+        glmvec3(-1.7f,  3.0f, -7.5f),
+        glmvec3( 1.3f, -2.0f, -2.5f),
+        glmvec3( 1.5f,  2.0f, -2.5f),
+        glmvec3( 1.5f,  0.2f, -1.5f),
+        glmvec3(-1.3f,  1.0f, -1.5f)
+    };
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
 
-  unsigned int VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-  glBindVertexArray(VAO);
-
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
-  glEnableVertexAttribArray(0);
-
-
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-
-  // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-  // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-  // glBindVertexArray(0);
-
-
-  // bind the VAO (it was already bound, but just to demonstrate): seeing as we only have a single VAO we can
-  // just bind it beforehand before rendering the respective triangle; this is another approach.
-  glBindVertexArray(VAO);
-
-  // uncomment this call to draw in wireframe polygons.
-  //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  // render loop
-  // -----------
-  while (!glfwWindowShouldClose(window)) {
-    // input
-    // -----
-    processInput(window);
-
-    // render
-    // ------
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // render container
-    ourShader.use();
     glBindVertexArray(VAO);
 
-    glDrawArrays(GL_TRIANGLE_FAN, 0, (vertices.size()/3)-1);
-        
-    //calculating delta time
-    float current_time = glfwGetTime();
-    delta_time = current_time - previous_frame;
-    previous_frame = current_time;
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    
-    // create transformations    
-    glm::vec3 direction = glm::vec3(0.0f, 0.0f, 0.0f);
-    //rotation transformation to our pacman character
-    if (pacman_direction == "right") {
-        rotation_angle = 0.0f;
-        direction = glm::vec3(1.0f, 0.0f, 0.0f);
-    }
-    else if (pacman_direction == "left") {        
-        rotation_angle = 180.0f;
-        direction = glm::vec3(-1.0f, 0.0f, 0.0f);
-    }
-    else if (pacman_direction == "down") {
-        rotation_angle = 90.0f;
-        direction = glm::vec3(0.0f, -1.0f, 0.0f);
-    }
-    else if (pacman_direction == "up") {
-        rotation_angle = -90.0f;
-        direction = glm::vec3(0.0f, 1.0f, 0.0f);
-    }
-        
-    //translation transformation to our pacman character
-    position = glm::translate(position, direction * speed * delta_time);           
-        
-    //then applying the rotation        
-    glm::mat4 transform = position;
-    transform = glm::rotate(transform, glm::radians(rotation_angle), glm::vec3(0.0f, 0.0f, 1.0f));
-    
-    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-    
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
+     position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5  sizeof(float), (void)0);
+    glEnableVertexAttribArray(0);
+     texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5  sizeof(float), (void)(3  sizeof(float)));
+    glEnableVertexAttribArray(1);
 
-  // optional: de-allocate all resources once they've outlived their purpose:
-  // ------------------------------------------------------------------------
-  glDeleteVertexArrays(1, &VAO);
-  glDeleteBuffers(1, &VBO);
 
-  // glfw: terminate, clearing all previously allocated GLFW resources.
-  // ------------------------------------------------------------------
-  glfwTerminate();
-  return 0;
+     load and create a texture 
+     -------------------------
+    unsigned int texture1, texture2;
+     texture 1
+     ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+     set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+     set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+     load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);  tell stb_image.h to flip loaded texture's on the y-axis.
+    unsigned char data = stbi_load(FileSystemgetPath(resourcestexturescontainer.jpg).c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        stdcout  Failed to load texture  stdendl;
+    }
+    stbi_image_free(data);
+     texture 2
+     ---------
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+     set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+     set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+     load image, create texture and generate mipmaps
+    data = stbi_load(FileSystemgetPath(resourcestexturesawesomeface.png).c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+         note that the awesomeface.png has transparency and thus an alpha channel, so make sure to tell OpenGL the data type is of GL_RGBA
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        stdcout  Failed to load texture  stdendl;
+    }
+    stbi_image_free(data);
+
+     tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+     -------------------------------------------------------------------------------------------
+    ourShader.use();
+    ourShader.setInt(texture1, 0);
+    ourShader.setInt(texture2, 1);
+
+
+     render loop
+     -----------
+    while (!glfwWindowShouldClose(window))
+    {
+         input
+         -----
+        processInput(window);
+
+         render
+         ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT  GL_DEPTH_BUFFER_BIT);  also clear the depth buffer now!
+
+          bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+         activate shader
+        ourShader.use();
+
+         create transformations
+        glmmat4 view          = glmmat4(1.0f);  make sure to initialize matrix to identity matrix first
+        glmmat4 projection    = glmmat4(1.0f);
+        projection = glmperspective(glmradians(45.0f), (float)SCR_WIDTH  (float)SCR_HEIGHT, 0.1f, 100.0f);
+        view       = glmtranslate(view, glmvec3(0.0f, 0.0f, -3.0f));
+         pass transformation matrices to the shader
+        ourShader.setMat4(projection, projection);  note currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        ourShader.setMat4(view, view);
+
+         render boxes
+        glBindVertexArray(VAO);
+        for (unsigned int i = 0; i  10; i++)
+        {
+             calculate the model matrix for each object and pass it to shader before drawing
+            glmmat4 model = glmmat4(1.0f);
+            model = glmtranslate(model, cubePositions[i]);
+            float angle = 20.0f  i;
+            model = glmrotate(model, glmradians(angle), glmvec3(1.0f, 0.3f, 0.5f));
+            ourShader.setMat4(model, model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+
+         glfw swap buffers and poll IO events (keys pressedreleased, mouse moved etc.)
+         -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+     optional de-allocate all resources once they've outlived their purpose
+     ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+
+     glfw terminate, clearing all previously allocated GLFW resources.
+     ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this
-// frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-
-  //inputs for the pacman character movement
-  if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-      pacman_direction = "up";
-  else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-      pacman_direction = "down";
-  else if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-      pacman_direction = "left";
-  else if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-      pacman_direction = "right";
-  else
-      pacman_direction = "none";
+ process all input query GLFW whether relevant keys are pressedreleased this frame and react accordingly
+ ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback
-// function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-  // make sure the viewport matches the new window dimensions; note that width
-  // and height will be significantly larger than specified on retina displays.
-  glViewport(0, 0, width, height);
+ glfw whenever the window size changed (by OS or user resize) this callback function executes
+ ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow window, int width, int height)
+{
+     make sure the viewport matches the new window dimensions; note that width and 
+     height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
