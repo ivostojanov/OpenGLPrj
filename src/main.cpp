5553a8306041c574d1fea4500224f6b63d959427
Afro <ivostojanov51@gmail.com>
2021-05-27 20:17:44 +0200

    lab06_zad01 - finished

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,42 @@ const std::string program_name = ("Camera");
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
 
 // camera
-static glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+// camera's position in world space
+static glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);   
+
+//the camera's target to look at will be the origin of the coordinate system
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+
+//returning the distance between the target and the camera position normalized gives us the camera direction
+glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+
+//upward direction of the camera
+static glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+
+//with the cross product we get the x axis of the camera
+glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+
+//retrieving the vector that points towards the y-axis
+glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+
+//this is the camera front vector z-axis positive points towards YOU
 static glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-static glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+//mouse movement
+float lastX = 400, lastY = 300;
+bool firstMouse = true;
+float yaw = 0;
+float pitch = 0;
+
+//jumping
+bool hasJumped = false;
 
 // timing
 static float deltaTime = 0.0f; // time between current frame and last frame
@@ -63,6 +90,12 @@ int main() {
   // -----------------------------
   glEnable(GL_DEPTH_TEST);
 
+  //capturing our mouse
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+  //assigning a mouse callback function
+  glfwSetCursorPosCallback(window, mouse_callback);
+
   // build and compile our shader program
   // ------------------------------------
 
@@ -75,6 +108,8 @@ int main() {
 
   // set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
+  std::vector<float> combinedVertices;
+
   float vertices[] = {
       -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f,
       0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
@@ -100,6 +135,26 @@ int main() {
       0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
       -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
 
+  float plain_floor[] = {
+      // X     Y      Z     T1    T2 
+      -1.0f, 0.0f, -1.0f, 0.0f, 0.0f,
+      1.0f,  0.0f, -1.0f, 1.0f, 0.0f,
+      -1.0f, 0.0f,  1.0f, 0.0f, 1.0f,
+
+      -1.0f, 0.0f,  1.0f, 0.0f, 1.0f, 
+      1.0f,  0.0f,  1.0f, 1.0f, 1.0f,
+      1.0f,  0.0f, -1.0f, 1.0f, 0.0f,
+  };
+
+  for (auto i = 0; i < 36*5; i++) {
+      combinedVertices.push_back(vertices[i]);
+  }
+
+  for (auto i = 0; i < 6 * 5; i++) {
+      plain_floor[i] = plain_floor[i] * 5;//quick scaling
+      combinedVertices.push_back(plain_floor[i]);
+  }
+
   // world space positions of our cubes
   glm::vec3 cubePositions[] = {
       glm::vec3(0.0f, 0.0f, 0.0f),    glm::vec3(2.0f, 5.0f, -15.0f),
@@ -115,7 +170,7 @@ int main() {
   glBindVertexArray(VAO);
 
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, combinedVertices.size()*sizeof(float), &combinedVertices[0], GL_STATIC_DRAW);
 
   // position attribute
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
@@ -226,25 +281,35 @@ int main() {
     // activate shader
     ourShader.use();
 
+    //calculating the camera target every frame based on our movement
+    //cameraFront is the direction the cameraIsActually looking z=-1 towards the scene
+    cameraTarget = cameraPos + cameraFront;
     // camera/view transformation
-    glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    glm::mat4 view = glm::lookAt(cameraPos, cameraTarget, cameraUp);
     ourShader.setMat4("view", view);
 
     // render boxes
     glBindVertexArray(VAO);
-    for (unsigned int i = 0; i < 10; i++) {
+    for (unsigned int i = 0; i < 1; i++) {
       // calculate the model matrix for each object and pass it to shader before
       // drawing
       glm::mat4 model = glm::mat4(
           1.0f); // make sure to initialize matrix to identity matrix first
       model = glm::translate(model, cubePositions[i]);
       float angle = 20.0f * i;
-      model =
-          glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+      //model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
       ourShader.setMat4("model", model);
 
       glDrawArrays(GL_TRIANGLES, 0, 36);
     }
+
+    //render floor    
+    glBindVertexArray(VAO);
+    glm::mat4 model = glm::mat4(
+        1.0f); // make sure to initialize matrix to identity matrix first
+    model = glm::translate(model, glm::vec3(0.0f,-0.5f,0.0f));
+    ourShader.setMat4("model", model);
+    glDrawArrays(GL_TRIANGLES, 36, 6);
 
     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
     // etc.)
@@ -271,17 +336,87 @@ void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
 
+  //we ensure that we have a consistent movement speed by using the time that has passed
   float cameraSpeed = 2.5f * deltaTime;
+
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && !hasJumped) {
+      hasJumped = true;
+      cameraPos.y = 1.5f;
+  }
+
+  bool ctrl_pressed = false;
+  if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS && !hasJumped) {
+      cameraPos.y = 0.0f;
+      cameraSpeed *= 0.5f;
+      ctrl_pressed = true;
+  }
+
+  //changing the camera position based on input
   if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
     cameraPos += cameraSpeed * cameraFront;
   if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     cameraPos -= cameraSpeed * cameraFront;
   if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    cameraPos -=
-        glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
   if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    cameraPos +=
-        glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+  //restrict to xz plane movement
+  if(!ctrl_pressed && !hasJumped)
+    cameraPos.y = 0.5f;
+
+  if (hasJumped) {
+      cameraPos.y -= deltaTime;
+      if (cameraPos.y < 0.5f) {
+          cameraPos.y = 0.5f;
+          hasJumped = false;
+      }          
+  }
+
+  //restricting the movement only on the floor (xz-axis constraint)
+  if (cameraPos.x < -5)
+      cameraPos.x = -5;
+  else if (cameraPos.x > 5)
+      cameraPos.x = 5;
+
+  if (cameraPos.z < -5)
+      cameraPos.z = -5;
+  else if (cameraPos.z > 5)
+      cameraPos.z = 5;
+
+}
+
+//function that listens to mouse inputs
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback
